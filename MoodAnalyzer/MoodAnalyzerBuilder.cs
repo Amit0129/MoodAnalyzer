10f65f1251a62378ecee8aa3bd5952488cad70d3
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-26 19:35:07 +0530

    ability to analyse and respond Happy or Sad Mood

---
+++ MoodAnalyzer/MoodAnalyzerBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    internal class MoodAnalyzerBuilder
+    {
+        public string  AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            if (message.ToUpper().Contains("GOOD"))
+            {
+                return "Good Mood";
+            }
+            return null;
+        }
+    }
+}
--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyzer Program");
+
+            MoodAnalyzerBuilder moodAnalyzerBuilder = new MoodAnalyzerBuilder();
+            
+            Console.WriteLine(moodAnalyzerBuilder.AnalyseMood("I am in a good mood"));
+            Console.WriteLine(moodAnalyzerBuilder.AnalyseMood("I am in a sad mood"));
         }
     }
 }
